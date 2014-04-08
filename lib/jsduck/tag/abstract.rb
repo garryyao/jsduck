259188278a9068af463ffe83f8b22f6cbe728cc5
Garry Yao <yaojun85@gmail.com>
2014-04-08 15:44:32 +0800

    Customized class icons for @abstract.

--- lib/jsduck/tag/abstract.rb
+++ lib/jsduck/tag/abstract.rb
@@ -4,6 +4,12 @@ module JsDuck::Tag
   class Abstract < BooleanTag
     def initialize
       @pattern = "abstract"
+      @class_icon = {
+          :small => File.dirname(__FILE__) + "/icons/abstract.png",
+          :large => File.dirname(__FILE__) + "/icons/abstract-large.png",
+          :redirect => File.dirname(__FILE__) + "/icons/abstract-redirect.png",
+          :priority => PRIORITY_SINGLETON,
+      }
       @signature = {:long => "abstract", :short => "ABS"}
       super
     end
